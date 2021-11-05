847b1734c3181fa96222ecfdcb17da42ed932d9f
MartinSMP <87109948+MartinSMP@users.noreply.github.com>
2021-11-04 20:34:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,94 +9,94 @@ function fisicaModulos(){
 
 }
 function silabusF(){
-    window.open("https://drive.google.com/file/d/1rwp_IniuBrsVT7X7VRaWufzQWz7K1bkj/view", "ventanafisica3")
+    window.open("https://drive.google.com/file/d/1rwp_IniuBrsVT7X7VRaWufzQWz7K1bkj/view", '_blank')
 }
 
 //FUNCIONES PARA COMPRENSION Y REDACCION DE TEXTOS 2
 
 function cpsZoom(){
-    window.open("https://canvas.utp.edu.pe/courses/226731/external_tools/1708", "ventacomprension1");
+    window.open("https://canvas.utp.edu.pe/courses/226731/external_tools/1708", '_blank');
 }
 
 function compreModulo(){
     
-    window.open("https://canvas.utp.edu.pe/courses/226731/modules", "ventanacomprension2");
+    window.open("https://canvas.utp.edu.pe/courses/226731/modules", '_blank');
 
 }
 
 function silabusC(){
-    window.open("https://drive.google.com/file/d/1RL4RlY-8Ao4HObC1RB6ydLBa9xFbC2At/view", "ventanaComprension3")
+    window.open("https://drive.google.com/file/d/1RL4RlY-8Ao4HObC1RB6ydLBa9xFbC2At/view", '_blank')
 }
 
 //FUNCIONES PARA MATEMATICA PARA INGENIEROS 1
 
 function claseMath(){
-    window.open("https://canvas.utp.edu.pe/courses/210937/external_tools/1708", "ventanaMate1");
+    window.open("https://canvas.utp.edu.pe/courses/210937/external_tools/1708", '_blank');
 }
 
 function modulsMath(){
     
-    window.open("https://canvas.utp.edu.pe/courses/210937", "ventanaMate2");
+    window.open("https://canvas.utp.edu.pe/courses/210937", '_blank');
 }
 
 function SilabusMath(){
-    window.open("https://drive.google.com/file/d/1dnKJqFYHJBHI_aPvKKeLZIDqchTPKYc8/view", "ventanaMate3")
+    window.open("https://drive.google.com/file/d/1dnKJqFYHJBHI_aPvKKeLZIDqchTPKYc8/view", '_blank')
 }
 
 //FUNCIONES PARA DIBUJO PARA INGENIEROS ZZZ
 
 function claseDibujo(){
-    window.open("https://canvas.utp.edu.pe/courses/226731/external_tools/1708", "ventanadibujo1");
+    window.open("https://canvas.utp.edu.pe/courses/226731/external_tools/1708", '_blank');
 }
 
 function modulsDibujo(){
     
-    window.open("https://canvas.utp.edu.pe/courses/226731/modules", "Ventanadibujo2");
+    window.open("https://canvas.utp.edu.pe/courses/226731/modules", '_blank');
 
 }
 function silabusDibujo(){
-    window.open("https://drive.google.com/file/d/1D0RgbHw__vl9RYZZ7xW0Lt2SJBqJBqSf/view", "ventanadibujo3")
+    window.open("https://drive.google.com/file/d/1D0RgbHw__vl9RYZZ7xW0Lt2SJBqJBqSf/view", '_blank')
 }
 
 //BOTONES DE PRINICPIOS DE ALGORITMOS
 
 function claseAlgoritmo(){
-    window.open("https://canvas.utp.edu.pe/courses/225555/external_tools/1708", "ventanaAlgoritmo1");
+    window.open("https://canvas.utp.edu.pe/courses/225555/external_tools/1708", '_blank');
 }
 
 
 function modulsAlgoritmo(){
     
-    window.open("https://canvas.utp.edu.pe/courses/225555", "ventanaAlgoritmo2");
+    window.open("https://canvas.utp.edu.pe/courses/225555", '_blank');
 
 }
 
 function silabusAlgoritmo(){
-    window.open("https://drive.google.com/file/d/1MkKN-9A2VL5SsYL8CYafDAaKJ0VxolWd/view","ventanaAlgoritmo3")
+    window.open("https://drive.google.com/file/d/1MkKN-9A2VL5SsYL8CYafDAaKJ0VxolWd/view",'_blank')
 }
 //Botones de Ingles
 
 function modulsIngles(){
     
-    window.open("https://canvas.utp.edu.pe/courses/215979", "ventanaingles");
+    window.open("https://canvas.utp.edu.pe/courses/215979", '_blank');
 
 }
 
 function silabusIngles(){
-    window.open("https://drive.google.com/file/d/1TVzwfQdWyo8EnFE3ikqM2euYmdxlQeF3/view", "ventanaIngles2")
+    window.open("https://drive.google.com/file/d/1TVzwfQdWyo8EnFE3ikqM2euYmdxlQeF3/view", '_blank')
 }
 //BOTONES ADICIONALES
 
 //CAMVAS
 function canvasUTP(){
-    window.open("https://canvas.utp.edu.pe/", "canvasUTP")
+    window.open("https://canvas.utp.edu.pe/", '_blank')
 }
 
 //PORTAL ESTUDIANTE
 function portalStudent(){
-    window.open("https://portalestudiante03.utp.edu.pe/Home/IndexUTP", "portalStudent")
+    window.open("https://portalestudiante03.utp.edu.pe/Home/IndexUTP", '_blank')
 }
 function talleres(){
-    window.open("https://portalestudiante03.utp.edu.pe/RecursosServicios/ListaTalleres", "talleres")
+    window.open("https://portalestudiante03.utp.edu.pe/RecursosServicios/ListaTalleres", '_blank')
 }
 
